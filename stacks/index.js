e99e522259c5f5d16aeeff642fc7615ab0dcb09f
Tej Rana <tej@allstrata.tech>
2021-10-16 08:52:19 +1000

    Setup react app

--- stacks/index.js
+++ stacks/index.js
@@ -1,6 +1,7 @@
 import StorageStack from "./StorageStack";
 import ApiStack from "./ApiStack";
 import AuthStack from "./AuthStack";
+import FrontendStack from "./FrontendStack";
 
 
 export default function main(app) {
@@ -16,8 +17,14 @@ export default function main(app) {
   });
 
 
-  new AuthStack(app, "auth", {
+  const authStack = new AuthStack(app, "auth", {
     api: apiStack.api,
+    bucket: storageStack.bucket,
+  });
+
+  new FrontendStack(app, "frontend", {
+    api: apiStack.api,
+    auth: authStack.auth,
     bucket: storageStack.bucket,
   });
 }
