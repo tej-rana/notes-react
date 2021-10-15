197ab6db2b87f107295ead9e7aee0a0672e27be1
Tej Rana <tej@allstrata.tech>
2021-10-16 06:25:56 +1000

    Added Billing API

--- stacks/ApiStack.js
+++ stacks/ApiStack.js
@@ -15,6 +15,7 @@ export default class ApiStack extends sst.Stack {
             defaultFunctionProps: {
                 environment: {
                     TABLE_NAME: table.tableName,
+                    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
                 },
             },
             routes: {
@@ -22,7 +23,8 @@ export default class ApiStack extends sst.Stack {
                 "GET    /notes/{id}": "src/get.main",
                 "GET    /notes": "src/list.main",
                 "PUT    /notes/{id}": "src/update.main",
-                "DELETE /notes/{id}": "src/delete.main"
+                "DELETE /notes/{id}": "src/delete.main",
+                "POST   /billing": "src/billing.main",
             },
         });
 
