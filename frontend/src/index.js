7f2b57aea45985b7e96acc03ff7285538f513bb6
Tej Rana <tej@allstrata.tech>
2021-10-16 17:38:52 +1000

    Added Loging and Signup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Amplify } from 'aws-amplify';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
